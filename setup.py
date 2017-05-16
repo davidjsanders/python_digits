a310af73b69533125ae0c2fb3692bbf89b20ad58
dsandersAzure <dsanders_can@outlook.com>
2017-05-15 20:47:02 -0400

    Promoted to version 1.0

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from setuptools import setup
 
 setup(name='python_digits',
-      version='0.4.2',
+      version='1.0',
       description='Python digits (integers between 0 and 9)',
       url='https://github.com/dsandersAzure/python_digits',
       author='David Sanders',
       author_email='dsanderscanadanospam@gmail.com',
       license='Apache License 2.0',
       packages=['python_digits'],
-      test_suite='nose.collector',
-      tests_require=['nose'],
       zip_safe=False)
